bdc7c790a8e6c3c992af0754992799023e9da7bd
Souparna Bose <souparnabose99@gmail.com>
2024-01-15 23:24:39 +0530

    Update project files

--- fast-api/passkey_integration/main.py
+++ fast-api/passkey_integration/main.py
@@ -45,6 +45,37 @@ async def get_login(request: Request):
         "login.html", {"request": request, "PROJECT_ID": PROJECT_ID}
     )
 
+
+@app.get("/profile", response_class=HTMLResponse)
+async def get_profile(request: Request):
+    # Acquire cookies with your preferred method
+    token: str = request.cookies.get(config.short_session_cookie_name) or ""
+    validation_result: SessionValidationResult = (
+        sessions.get_and_validate_short_session_value(short_session=token)
+    )
+    if validation_result.authenticated:
+
+        emailList: List[Identifier] = identifiers.list_all_emails_by_user_id(
+            user_id=validation_result.user_id
+            or ""  # at this point user_id should be non empty string since user was authenticated
+        )
+
+        context = {
+            "request": request,
+            "PROJECT_ID": PROJECT_ID,
+            "USER_ID": validation_result.user_id,
+            "USER_NAME": validation_result.full_name,
+            "USER_EMAIL": emailList[0].value,
+        }
+        return templates.TemplateResponse("profile.html", context)
+
+    else:
+        return Response(
+            content="You are not authenticated or have not yet confirmed your email.",
+            status_code=401,
+        )
+
+
 if __name__ == "__main__":
     pass
 
