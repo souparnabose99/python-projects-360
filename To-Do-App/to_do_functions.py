1d41bd6397e4ddb02b86897aa2f37d967079b561
Souparna Bose <souparnabose99@gmail.com>
2024-05-28 22:08:00 +0530

    Add To-Do-App

--- To-Do-App/to_do_functions.py
+++ To-Do-App/to_do_functions.py
@@ -1,1 +1,19 @@
-FILEPATH = "todo_items.txt"
+FILEPATH = r"Data\todo_items.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return a list of to-do items
+    """
+    with open(filepath, "r") as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todo_items, filepath=FILEPATH):
+    """
+    Write the to-do items in a text file
+    """
+    with open(filepath, "w") as file:
+        file.writelines(todo_items)
+
