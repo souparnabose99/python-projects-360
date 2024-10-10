a373a5bb6c9b6369096083c71d3c11a46dbb5b28
Souparna Bose <souparnabose99@gmail.com>
2024-10-10 16:46:45 +0530

    Update project files

--- streamlit/streamlit_intro/main.py
+++ streamlit/streamlit_intro/main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.write("Streamlit Introduction")
+st.title("Streamlit")
+st.header("UI Functionality")
+
+btn_1 = st.button("Func-1")
+print(btn_1)
+
+btn_2 = st.button("Func-2")
+print(btn_2)
