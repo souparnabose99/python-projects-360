52cd1b876bf120ef92f5f2a765e4f5ca8c2fab9a
Souparna Bose <souparnabose99@gmail.com>
2024-11-01 14:31:01 +0530

    Update project files

---
+++ asyncio/data_crawling_asyncio/async_api_models.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+from enum import StrEnum
+
+
+class Types(StrEnum):
+    JOB = 'job'
+    STORY = 'story'
+    COMMENT = 'comment'
+
+
+@dataclass
+class BaseClass:
+    id: int
+    type: Types
+    deleted: bool = False
+    by: str = 'anon'
+    time: int = 0
+    dead: bool = False
+    kids: list[int] = field(default_factory=list)
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+@dataclass
+class Story(BaseClass):
+    descendants: int = 0
+    title: str = ''
+    url: str = 'https://news.ycombinator.com/'
+    text: str = ''
+    score: int = 0
+
+
+
