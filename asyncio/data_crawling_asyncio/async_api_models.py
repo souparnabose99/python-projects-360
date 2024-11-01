02e0a46b78ed091594b4ec4c4192696f314f1ace
Souparna Bose <souparnabose99@gmail.com>
2024-11-01 14:33:19 +0530

    Update project files

--- asyncio/data_crawling_asyncio/async_api_models.py
+++ asyncio/data_crawling_asyncio/async_api_models.py
@@ -31,4 +31,29 @@ class Story(BaseClass):
     score: int = 0
 
 
+@dataclass
+class Job(BaseClass):
+    title: str = ''
+    text: str = ''
+    score: int = 0
+    url: str = 'https://news.ycombinator.com/'
+
+
+@dataclass
+class Comment(BaseClass):
+    parent: int = 0
+    text: str = ''
+
+
+@dataclass
+class User:
+    id: str
+    created: int
+    karma: int = 0
+    about: str = ''
+    delay: int = 0
+    submitted: list[int] = field(default_factory=list)
+
+    def __str__(self):
+        return f'{self.id}'
 
