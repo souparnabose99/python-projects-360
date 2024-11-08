0072c35aa92476f927f5f369bc34ab53f3b9c95e
Souparna Bose <souparnabose99@gmail.com>
2024-11-08 15:58:01 +0530

    Update project files

--- file_errors/file_errors.py
+++ file_errors/file_errors.py
@@ -3,11 +3,22 @@ from pathlib import Path
 def open_file():
     path = Path(__file__).parent
     path = path / "sample_path" / "dummy.txt"
-    path.open("r")
+
+    try:
+        file = path.open("r")
+        content = file.read()
+        print(content)
+        file.close()
+    except FileNotFoundError:
+        print(f"{path} does not exist")
 
 
 def main():
     open_file()
 
+
 if __name__ == "__main__":
     main()
+
+# Output
+# E:\file_errors\sample_path\dummy.txt does not exist
