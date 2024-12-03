771699f490e7dfc317ea19ee05e4df492d143772
Souparna Bose <souparnabose99@gmail.com>
2024-12-03 16:14:19 +0530

    Update project files

--- pypdf2/page_removal.py
+++ pypdf2/page_removal.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
     input_pdf_path = "Downloads/abc.pdf"
     # path to the output PDF
     output_pdf_path = "Downloads/abc_edited.pdf"
-    pages_to_remove = [2,3,6,9,15,19,21,23,33,64,75,76]
+    pages_to_remove = [2, 9, 21, 75]
     pages_to_remove = [x - 1 for x in pages_to_remove]
     # -1 bcz it works on 0-index
-    remove_pdf_pages(input_pdf_path, output_pdf_path, pages_to_remove)
+    remove_selective_pdf_pages(input_pdf_path, output_pdf_path, pages_to_remove)
     print(f"Pages {pages_to_remove} removed from {input_pdf_path} and saved to {output_pdf_path}")
+
