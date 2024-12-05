7ef0162263c4fbf3c5c09bed7e3ae35bbd0ff0eb
Souparna Bose <souparnabose99@gmail.com>
2024-12-05 16:29:29 +0530

    Update project files

--- ssh_with_fabric/ssh_script_with_fabric.py
+++ ssh_with_fabric/ssh_script_with_fabric.py
@@ -2,5 +2,5 @@ from fabric import Connection, Config
 
 
 config = Config(overrides={"sudo": {"password": "xxx112233"}})
-conn = Connection("mike@10.10.166.128:22", connect_kwargs={"password": "xxx112233"}, config=config)
+conn = Connection("bose@10.10.166.128:22", connect_kwargs={"password": "xxx112233"}, config=config)
 
