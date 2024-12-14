4c9ba0c2e191dbe6284b0344b2d066f48d483002
Souparna Bose <souparnabose99@gmail.com>
2024-12-14 15:58:14 +0530

    Update project files

--- fast-api/ml_fast_api/main.py
+++ fast-api/ml_fast_api/main.py
@@ -1,16 +1,39 @@
 from fastapi import FastAPI
 import uvicorn
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def main():
-    return {"message": "Base Url"}
+class RequestBody(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
 
-# /name endpoint
-@app.get("/{name}")
-def page_name(name: str):
-    return {"message": f"Welcome to !, {name}"}
+iris = load_iris()
 
+
+X = iris.data
+Y = iris.target
+
+
+clf = GaussianNB()
+clf.fit(X, Y)
+
+
+@app.post('/predict')
+def predict(data: request_body):
+    test_data = [[
+        data.sepal_length,
+        data.sepal_width,
+        data.petal_length,
+        data.petal_width
+    ]]
+
+    class_idx = clf.predict(test_data)[0]
+    return {'class': iris.target_names[class_idx]}
