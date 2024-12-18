505e4662a0ee2f1d9d322db6f08ca5a4c4f73192
Souparna Bose <souparnabose99@gmail.com>
2024-12-18 16:30:28 +0530

    Update project files

--- fast-api/penguins_classification_fast_api/src/async_context_manager.py
+++ fast-api/penguins_classification_fast_api/src/async_context_manager.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 ml_models = {}
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Set up the ML model here
-
+    data = pd.read_csv('penguins.csv')
+    data = data.dropna()
+    le = preprocessing.LabelEncoder()
+    X = data[["bill_length_mm", "flipper_length_mm"]]
+    le.fit(data["species"])
+    y = le.transform(data["species"])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+    clf = Pipeline(
+        steps=[("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11))]
+    )
+    clf.set_params().fit(X_train, y_train)
+    ml_models["clf"] = clf
+    ml_models["le"] = le
     yield
     # Clean up the models and release resources
     ml_models.clear()
